53794bd15f078d3e02233a61585b26cc670c91e5
worace <horace.d.williams@gmail.com>
2015-02-23 10:31:33 -0700

    add test code so far for mocking apis

--- app/controllers/tweet_streams_controller.rb
+++ app/controllers/tweet_streams_controller.rb
@@ -8,9 +8,6 @@ class TweetStreamsController < ApplicationController
   end
 
   def twitter_client
-    @twitter_client ||= Twitter::REST::Client.new do |config|
-                          config.consumer_key        = "W94h9TI21dRmkuDKewew2gy2t"
-                          config.consumer_secret     = "4QOGnSWrT8vfxoUHohZFEtPFeCGlo47uhJatjL90BD73JGe3g7"
-                        end
+    TWITTER
   end
 end
--- test/controllers/tweet_streams_controller_test.rb
+++ test/controllers/tweet_streams_controller_test.rb
@@ -2,6 +2,15 @@ require "test_helper"
 
 class TweetStreamsControllerTest < ActionController::TestCase
   test "fetches tweets on create" do
+    # stub twitter client to modify return of user_timeline
+    # method
+    # array of "tweet" objects
+    # interface: #user -> #screen_name, #text -> "some string"
+    # stub("label", :hash => "of", :methods => "to implement")
+    @controller.twitter_client
+      .expects(:user_timeline)
+      .with("j3")
+      .returns([stub("Tweet", message: "heres a AALLL CAPS tweet", user: stub(screen_name: "j3"))])
     post :create, :twitter_handle => "j3"
     assert_response :success
     assert_not_nil assigns(:tweets)
