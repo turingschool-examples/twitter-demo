e0b276b49607dedcf4d3f6315a1aecb3ff967370
worace <horace.d.williams@gmail.com>
2015-03-31 13:40:38 -0600

    remove test helper require statements -- getting ahead of ourselves

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'mocha/mini_test'
-require 'vcr'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -10,10 +8,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
-end
-
-VCR.configure do |c|
-  c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
-  c.hook_into :webmock
-  c.default_cassette_options = { :serialize_with => :json }
 end
