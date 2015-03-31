bcbe38ec1f7abe199adcb0c06126086ccf34e9ad
worace <horace.d.williams@gmail.com>
2015-03-31 12:24:44 -0600

    remove implementations from past session

--- app/controllers/tweet_streams_controller.rb
+++ app/controllers/tweet_streams_controller.rb
@@ -4,10 +4,9 @@ class TweetStreamsController < ApplicationController
   end
 
   def create
-    @tweets = twitter_client.fetch_tweets(params[:twitter_handle])
-  end
-
-  def twitter_client
-    @twitter_client ||= TwitterClient.new
+    @tweets = Twitter::REST::Client.new do |config|
+                config.consumer_key        = "W94h9TI21dRmkuDKewew2gy2t"
+                config.consumer_secret     = "4QOGnSWrT8vfxoUHohZFEtPFeCGlo47uhJatjL90BD73JGe3g7"
+              end.user_timeline(params[:twitter_handle])
   end
 end
